e72073018c2a91a14062be80f572998f55d5f36f
Erin <ed878321@ohio.edu>
2024-05-17 19:17:58 +0100

    Add git-lab-program.cc with Hello Git message

---
+++ git-lab-program.cc
@@ -0,0 +1,17 @@
+/*
+*   File: git-lab-program.cc
+*   Author: Erin Demagall
+*   Date: 05-17-2024
+*Description: Project 1 of CS2400 summer session
+*/
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
